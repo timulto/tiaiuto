fe1fcf9ca439450533e693fca458f1adb9798143
raffaele guidi <raffaele.p.guidi@gmail.com>
2015-05-14 00:49:00 +0200

    draft crawling of calendar items

--- tiaiuto.js
+++ tiaiuto.js
@@ -5,30 +5,62 @@ if (Meteor.isClient) {
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
+    },
+    progetti: function () {
+      return Session.get('progetti');
     }
   });
 
   Template.hello.events({
     'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+        console.log("eccomi");
+        Meteor.call("getProgetti", function(err, res){
+            if (!err) {
+                Session.set("progetti", res);
+            }
+        })
+      }
+   });
+}
+
+if (Meteor.isClient) {
+    Meteor.startup(function () {
+        Meteor.call("getProgetti", function(err, res){
+            if (!err) {
+                Session.set("progetti", res);
+            }
+        })
+    });
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
-        var res = HTTP.get("http://www.romaltruista.it/calendario.asp");
-        var progetti = new Array();
-        if (res.statusCode == 200) {
-            var $ = cheerio.load(res.content);
-            var progettiaperti = $('.attivita.progettoaperto');
-            console.log("progetti aperti %d", progettiaperti.length);
-            progettiaperti.each(function (i, elem) {
-                console.log($(this).find('.titolo').text());
-                progetti.push({ titolo: $(this).find('.titolo').text() });
-            });
-            console.log(JSON.stringify(progetti));
-        }
+        //noop
+        Meteor.methods({
+            getProgetti: function(){
+                // meteor add http aldeed:http
+                // https://github.com/aldeed/meteor-http-extras/
+                // to fix encoding issues
+                var res = HTTP.get("http://www.romaltruista.it/calendario.asp");
+                // http://www.romaltruista.it/opportunita.asp
+                if (res.statusCode == 200) {
+                    var $ = cheerio.load(res.content);
+                    var progettiaperti = $('.attivita.progettoaperto');
+                    var progetti = new Array();
+
+                    console.log("progetti aperti %d", progettiaperti.length);
+                    progettiaperti.each(function (i, elem) {
+                        //console.log($(this).find('.titolo').text());
+                        progetti.push({
+                            titolo: $(this).find('.titolo').text(),
+                            orari: $(this).find('.orari').text(),
+                        });
+                    });
+                    return progetti;
+                } else {
+                    return null;
+                }
+            }
+        });
     });
 }
