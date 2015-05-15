2f6002632929ee7feb28ec963970e8fe79eeefaf
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-05-15 17:24:58 +0200

    caricamento e display da database

--- tiaiuto.js
+++ tiaiuto.js
@@ -1,52 +1,41 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.listaProgetti.helpers({
-    counter: function () {
-      return Session.get('counter');
-    },
-    progetti: function () {
-      return Session.get('progetti');
-    }
-  });
-
-  Template.listaProgetti.events({
-    'click button': function () {
-        console.log("eccomi");
-        Meteor.call("getProgetti", function(err, res){
-            if (!err) {
-                Session.set("progetti", res);
-            }
-        })
-      }
-   });
-}
+Opportunità = new Mongo.Collection("opportunità");
 
 if (Meteor.isClient) {
+    Template.listaOpportunità.helpers({
+        listaOpportunità: function () {
+             return Opportunità.find({});
+        }
+    });
+
+    Template.listaOpportunità.events({
+        'click button': function () {
+            console.log("eccomi");
+        }
+    });
+
     Meteor.startup(function () {
-        Meteor.call("getProgetti", function(err, res){
-            if (!err) {
-                Session.set("progetti", res);
-            }
-        });
-
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
     });
 }
 
-
-
-
 if (Meteor.isServer) {
 
-    function getOpportunita() {
+    function aggiungiOpportunità(opportunità){
+        opportunità.aggiornamento = new Date();
+        if (Opportunità.find({url: opportunità.url}).count() > 0) {
+            Opportunità.remove({url: opportunità.url});
+//            console.log('removing existing %s', opportunità.url);
+        }
+        Opportunità.insert(opportunità);
+//        console.log('adding %s', opportunità.url);
+    }
+
+    function crawlRomaltruista() {
         var res = HTTP.getWithEncoding("http://www.romaltruista.it/opportunita.asp", {encoding: {from: "utf-8", to: "iso-8859-1"}});
         if (res.statusCode == 200) {
             var $ = cheerio.load(res.content);
             var opportunita = $('table[width="100%"]');
-            var opportunitaArr = new Array();
 
             console.log("scaricate opportunità %d", opportunita.length);
             opportunita.each(function (i, elem) {
@@ -59,53 +48,45 @@ if (Meteor.isServer) {
                     var descrizione = $(this.find('td[rowspan="2"]'));
                     var linkDettaglio = $(this.find('img[style="cursor: pointer"]')).attr('onclick');
 
-                    /*
-                    <td align="right" style="font-size: 1em;">
-                        <img style="cursor: pointer" onclick="javascript:apriPopUp('opportunita_dettaglio.asp?idprogetto=13306&amp;wname='+window.name,'Dettaglio Progetto');" src="sites/default/themes/honbiro/opp_desc.jpg">
-                    </td>
-                    */
-
-                    opportunitaArr.push({
+                    aggiungiOpportunità({
                         titolo: titolo.text().trim(),
                         area: area.html().substring(area.html().indexOf("Area:")+5).trim(),
                         zona: zona.text().trim(),
                         quando: quando.text().trim(),
                         orari: orari.text().trim(),
                         descrizione: descrizione.text().trim(),
-                        linkDettaglio: "http://www.romaltruista.it/" +
-                                        linkDettaglio.substring(
-                                            "javascript:apriPopUp('".length,
-                                            linkDettaglio.indexOf('&wname')
-                                        )
+                        fonte: "romaltruista",
+                        url: "http://www.romaltruista.it/" +
+                                linkDettaglio.substring(
+                                    "javascript:apriPopUp('".length,
+                                    linkDettaglio.indexOf('&wname')
+                                )
                     });
                 }
             });
-            return opportunitaArr;
-        } else {
-            return null;
+            console.log("fine caricamento su db - %d opportunità nel db", Opportunità.find({}).count());
         }
     }
 
     SyncedCron.add({
-      name: 'Crawling progetti',
+      name: 'Crawling romaltruista',
       schedule: function(parser) {
         // parser is a later.parse object
         return parser.text('every 2 hours');
       },
       job: function() {
-          opps = getOpportunita();
+          crawlRomaltruista();
       }
     });
 
     Meteor.startup(function () {
-        opps = getOpportunita();
-
+        crawlRomaltruista();
         SyncedCron.start();
 
-        Meteor.methods({
-            getProgetti: function() {
-                return opps;
-            }
-        });
+//        Meteor.methods({
+//            getProgetti: function() {
+//                return opps;
+//            }
+//        });
     });
 }
